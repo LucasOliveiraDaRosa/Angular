fc992eb1bf80f55093cf6e3ea632e17779387367
Lucas Oliveira da Rosa <lucasdarosasd@gmail.com>
2022-03-08 17:08:01 -0300

    Projeto concluido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
-import { MatSidenavModule} from '@angular/material/sidenav';
-import { MatCardModule} from '@angular/material/card';
-import { MatListModule} from '@angular/material/list';
+import { MatSidenavModule } from  '@angular/material/sidenav';
+import { MatCardModule } from  '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { MatButtonModule } from  '@angular/material/button';
+import { MatSnackBarModule } from  '@angular/material/snack-bar';
+import { HttpClientModule } from  '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 import { RedDirective } from './directives/red.directive';
-import { MatButtonModule} from '@angular/material/button';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from  '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -27,7 +45,10 @@ import { MatButtonModule} from '@angular/material/button';
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
-    RedDirective
+    ProductReadComponent,
+    RedDirective,
+    ProductUpdateComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,9 +58,20 @@ import { MatButtonModule} from '@angular/material/button';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
